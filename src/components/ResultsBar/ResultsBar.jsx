4b7f4f3cc1dcc651490cdf3b869f5fba828d8556
Rebecca-123 <rebecca.notw@gmail.com>
2024-01-04 22:22:32 -0800

    refactor callback functions with useCallback

--- src/components/ResultsBar/ResultsBar.jsx
+++ src/components/ResultsBar/ResultsBar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useCallback } from "react";
 import "./ResultsBar.css";
 import ResultCard from "../ResultCard/ResultCard";
 import { Box, Flex, Text } from "@chakra-ui/react";
@@ -14,42 +14,45 @@ export default function ResultsBar({
 }) {
   const { data } = useContext(DataContext);
   const { user } = UserAuth();
+  const filterItem = useCallback((item) => {
+    return (
+    (search.toLowerCase() === "" ||
+      item.name.toLowerCase().includes(search)) &&
+    (findFilter.islost === item.islost ||
+      findFilter.isFound === !item.islost) &&
+    (findFilter.type === "everything" || findFilter.type === item.type) &&
+    (findFilter.uploadDate === "" ||
+      (item.itemdate && item.itemdate.includes(findFilter.uploadDate))) &&
+    (!findFilter.isYourPosts ||
+      (findFilter.isYourPosts && item.email === user.email)) &&
+    (findFilter.isShowReturned || !item.isresolved)
+  );
+}, [search, findFilter.isFound, findFilter.isShowReturned, findFilter.isYourPosts, findFilter.islost, findFilter.type, findFilter.uploadDate, user]);
+
+  const mapItem = useCallback((item) => {
+    return (
+      <Box
+        key={item.location}
+        onClick={() => {
+          setFocusLocation(item.location);
+        }}
+        _hover={{
+          transform: "scale(0.99)",
+        }}
+      >
+        <ResultCard
+          props={item}
+          setData={setData}
+          onResultsBarClose={onResultsBarClose}
+          setLeaderboard={setLeaderboard}
+        />
+      </Box>
+    );
+  }, [setFocusLocation, onResultsBarClose, setData, setLeaderboard]);
 
   const allResults = data
-    .filter((item) => {
-      return (
-        (search.toLowerCase() === "" ||
-          item.name.toLowerCase().includes(search)) &&
-        (findFilter.islost === item.islost ||
-          findFilter.isFound === !item.islost) &&
-        (findFilter.type === "everything" || findFilter.type === item.type) &&
-        (findFilter.uploadDate === "" ||
-          (item.itemdate && item.itemdate.includes(findFilter.uploadDate))) &&
-        (!findFilter.isYourPosts ||
-          (findFilter.isYourPosts && item.email === user.email)) &&
-        (findFilter.isShowReturned || !item.isresolved)
-      );
-    })
-    .map((item) => {
-      return (
-        <Box
-          key={item.location}
-          onClick={() => {
-            setFocusLocation(item.location);
-          }}
-          _hover={{
-            transform: "scale(0.99)",
-          }}
-        >
-          <ResultCard
-            props={item}
-            setData={setData}
-            onResultsBarClose={onResultsBarClose}
-            setLeaderboard={setLeaderboard}
-          />
-        </Box>
-      );
-    });
+    .filter(filterItem)
+    .map(mapItem);
 
   const noResults = (
     <Flex
