bcf8b11afa411a6e6ac7c8c970322f652866e1ac
Dang Nguyen Nguyen <93059380+NwinNwin@users.noreply.github.com>
2024-02-20 11:33:37 -0800

    removed unused useCallback

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 import { UserAuth } from "../../context/AuthContext";
