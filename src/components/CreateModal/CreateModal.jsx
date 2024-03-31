8ee070be64b53ecf01b28160cba62ac1e9907b0c
Steven Zhou <stevenzhouni@gmail.com>
2024-03-30 22:30:29 -0700

    eslint

--- src/components/CreateModal/CreateModal.jsx
+++ src/components/CreateModal/CreateModal.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect, useCallback, useRef } from "react";
+import { useState, useEffect, useCallback } from "react";
 import {
   Image,
   Button,
